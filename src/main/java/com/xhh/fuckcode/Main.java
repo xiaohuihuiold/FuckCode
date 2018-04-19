0a1f6ad55921d2f1f2f215cae275deeab076bddc
xiaohuihuiold <xiaohuihuiold@gmail.com>
2018-04-19 22:30:39 +0800

    添加倒三角和斐波拉契数列demo

--- src/main/java/com/xhh/fuckcode/Main.java
+++ src/main/java/com/xhh/fuckcode/Main.java
@@ -16,63 +16,10 @@ public class Main {
         Runtime runtime = Runtime.getInstance();
         try {
             fuckedLoader.load(Main.class.getResourceAsStream("/Fibonacci.js"));
-            //runtime.setFunBlocks(test());
             runtime.run();
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("读取资源失败:" + e.getMessage());
         }
-    }
-
-    public String pr() {
-        System.out.println("HelloWorld!");
-        return "测试";
-    }
-
-    public static ArrayList<FunBlock> test() {
-        ArrayList<FunBlock> funBlocks = new ArrayList<>();
-
-        FunBlock main = new FunBlock();
-        main.setName("main");
-        main.addLine(new MovLine("v0", "@小灰灰"));
-        main.addLine(new MovLine("v1", "v0"));
-        main.addLine(new InvLine("println", new Object[]{"v1"}));
-        main.addLine(new InsLine("v2", "$com.xhh.fuckcode.Main"));
-        main.addLine(new InvLine("test", new Object[]{"v2"}));
-        funBlocks.add(main);
-
-        FunBlock test = new FunBlock();
-        test.setParams(new Object[]{"v0"});
-        test.setName("test");
-        test.addLine(new InvLine("v0", "pr", new Object[]{"@test"}, false));
-        test.addLine(new MovLine("v3", 200));
-        test.addLine(new MovLine("v4", 70.33));
-        test.addLine(new DataLine(DataLine.TYPE.MUL, "v2", "v3", "v4"));
-        test.addLine(new DataLine(DataLine.TYPE.ADD, "v1", "v3", "*"));
-        test.addLine(new DataLine(DataLine.TYPE.ADD, "v1", "v1", "v4"));
-        test.addLine(new DataLine(DataLine.TYPE.ADD, "v1", "v1", "@结果"));
-        test.addLine(new DataLine(DataLine.TYPE.ADD, "v2", "v1", "v2"));
-        test.addLine(new InvLine("println", new Object[]{"v2"}));
-
-        test.addLine(new MovLine("v6", true));
-        test.addLine(new MovLine("v5", 0));
-
-        WhileBlock whileBlock = new WhileBlock("v6");
-        whileBlock.addLine(new DataLine(DataLine.TYPE.ADD, "v2", "@v5的值:", "v5"));
-
-        whileBlock.addLine(new LogicLine(LogicLine.TYPE.LESS_EQUAL, "v9", "v5", 50));
-        LogicBlock logicBlock = new LogicBlock("v9");
-        logicBlock.addLine(new InvLine("println", new Object[]{"v2"}));
-        whileBlock.addLine(logicBlock);
-
-        //whileBlock.addLine(new InvLine("println", new Object[]{"v2"}));
-        whileBlock.addLine(new DataLine(DataLine.TYPE.ADD, "v5", "v5", 1));
-        whileBlock.addLine(new LogicLine(LogicLine.TYPE.GREATER_EQUAL, "v6", 10, "v5"));
-
-        test.addLine(whileBlock);
-
-        funBlocks.add(test);
-
-        return funBlocks;
     }
 }
